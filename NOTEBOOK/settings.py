0da1fa159e1eb04d27e46c8630dfb38b298f6c41
J AJAY NAYAK <jajaynayak34@gmail.com>
2024-07-04 10:43:50 +0530

    Update ALLOWED_HOSTS in settings.py

--- NOTEBOOK/settings.py
+++ NOTEBOOK/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = 'django-insecure-ty@@+v(*6!ne47zk-$qiy(-$9k1dv&wt%&+&3y8k7)j*6w(0**
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app']
 
 # Application definition
 INSTALLED_APPS = [
